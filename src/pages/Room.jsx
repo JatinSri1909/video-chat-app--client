476adbacdf7c5707c012008578bd16f368b8c9fd
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-09-28 16:50:54 +0530

    Created Peer.jsx and created offer logic

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,18 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import { useSocket } from '../providers/Socket'
+import { usePeer } from '../providers/Peer';
 
 const Room = () => {
 
     const { socket } = useSocket();
+    const { peer, createOffer } = usePeer();
 
-    const handleNewUserJoined = (data) => {
+    const handleNewUserJoined = useCallback(async(data) => {
         const { emailId } = data;
         console.log("User", emailId, "joined the room");
-    }
+        const offer = await createOffer();
+        socket.emit("call-user", { emailId, offer });
+    }, [createOffer, socket]);
 
     useEffect(() => {
         socket.on('user-joined', handleNewUserJoined);
-    }, [socket]);
+        return () => {
+            socket.off('user-joined', handleNewUserJoined);
+        };
+    }, [socket, handleNewUserJoined]);
 
   return (
     <div className='room-page-container'>
