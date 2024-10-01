92a9ee1ec1387db512d0c4a90ba7efb613734ef0
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-02 02:25:17 +0530

    Refactor PeerService to add getOffer method for creating and setting local description

--- src/providers/Peer.js
+++ src/providers/Peer.js
@@ -18,15 +18,24 @@ class PeerService {
     if (this.peer) {
       await this.peer.setRemoteDescription(offer);
       const ans = await this.peer.createAnswer();
-      await this.peer.setLocalDescription( new RTCSessionDescription(ans));
+      await this.peer.setLocalDescription(new RTCSessionDescription(ans));
       return ans;
     }
   }
 
   async setLocalDescription(ans) {
     if (this.peer) {
-        await this.peer.setRemoteDescription( new RTCSessionDescription(ans));
+      await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
     }
   }
 
+  async getOffer() {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+      return offer;
+    }
+  }
 }
+
+export default new PeerService();
