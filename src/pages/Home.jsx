5ea9b5227690327c1f1c47095e6e570a1698a95c
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-02 02:45:51 +0530

    Refactor Home component to use useCallback for handling room joining and remove unused email variable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,37 +15,45 @@ const Home = () => {
       socket.emit("room:join", { email, room });
     },
     [email, room, socket]
-  )
+  );
 
   const handleJoinRoom = useCallback(
     (data) => {
       const { room } = data;
       navigate(`/room/${room}`);
-    },[navigate]);
+    },
+    [navigate]
+  );
 
-    useEffect(() => {
-      socket.on("room:join", handleJoinRoom);
-      return () => {
-        socket.off("room:join", handleJoinRoom);
-      };
-    },[socket, handleJoinRoom]);
+  useEffect(() => {
+    socket.on("room:join", handleJoinRoom);
+    return () => {
+      socket.off("room:join", handleJoinRoom);
+    };
+  }, [socket, handleJoinRoom]);
 
   return (
     <div className="homepage-container">
       <div className="input-container">
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="Enter your email here...."
-        />
-        <input
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-          type="text"
-          placeholder="Enter Room "
-        />
-        <button onClick={handleJoinRoom}>Join</button>
+        <form onSubmit={handleSubmitForm}>
+          <label htmlFor="email">Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            id="email"
+            placeholder="Enter your email here...."
+          />
+          <label htmlFor="room">Room Number</label>
+          <input
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+            type="text"
+            id="room"
+            placeholder="Enter Room "
+          />
+          <button onClick={handleJoinRoom}>Join</button>
+        </form>
       </div>
     </div>
   );
