9960ff82a2998aa4e46e8922a62f7a8d8d7e8f4d
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-02 02:29:35 +0530

    Refactor SocketProvider to use a different host URL

--- src/providers/Socket.jsx
+++ src/providers/Socket.jsx
@@ -3,20 +3,19 @@ import { io } from "socket.io-client";
 
 const SocketContext = React.createContext(null);
 
-const host = process.env.REACT_APP_HTTP_HOST || "http://localhost:5000";
+const host = "http://localhost:8080";
 
 export const useSocket = () => {
-  return React.useContext(SocketContext);
+  const socket = React.useContext(SocketContext)
+  return socket;
 };
 
 export const SocketProvider = (props) => {
   const socket = useMemo(() => io(host), []);
 
   return (
-    <div>
-      <SocketContext.Provider value={{ socket }}>
+      <SocketContext.Provider value={ socket }>
         {props.children}
       </SocketContext.Provider>
-    </div>
   );
 };
