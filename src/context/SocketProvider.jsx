bcb2e83d9ef00f21ca5ef3904a2c47129db3b4ab
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-04 02:49:40 +0530

    Refactor host variable in SocketProvider component

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 import React, { useMemo } from "react";
 
+const host = process.env.REACT_APP_HTTP_HOST ||"http://localhost:8080";
+
 export const SocketContext = React.createContext(null);
 
 export const useSocket = () => {
