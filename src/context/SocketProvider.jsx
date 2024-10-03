c01f64dbed6b8826283d1c2ddabb9629120ce0f6
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-04 02:53:44 +0530

    Refactor host variable in SocketProvider component

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -11,7 +11,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("http://localhost:8080"), []);
+  const socket = useMemo(() => io(host), []);
 
   return (
     <SocketContext.Provider value={socket}>
